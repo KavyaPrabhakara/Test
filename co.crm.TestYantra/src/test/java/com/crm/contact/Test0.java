cf9e462050c4b85ac95bcb0fb8e24282ee494f29
Tejaswini <Tejaswini@Kavya>
2021-12-30 15:06:34 +0530

    hii

---
+++ co.crm.TestYantra/src/test/java/com/crm/contact/Test0.java
@@ -0,0 +1,20 @@
+package com.crm.contact;
+
+import org.testng.annotations.Test;
+
+public class Test0 {
+	@Test
+	public void createContact() {
+		System.out.println("Create Contact");
+	}
+	
+	@Test
+	public void modifyContact() {
+		System.out.println("Modify Contact");
+	}
+	
+	@Test
+	public void deleteContact() {
+		System.out.println("Delete Contact");
+	}
+}
---
+++ co.crm.TestYantra/src/test/java/com/crm/org/Test0.java
@@ -0,0 +1,21 @@
+package com.crm.org;
+
+import org.testng.annotations.Test;
+
+public class Test0 {
+	@Test
+	public void createOrg() {
+		System.out.println("Create Organization");
+	}
+	
+	@Test
+	public void modifyOrg() {
+		System.out.println("Modify Organization");
+	}
+	
+	@Test
+	public void deleteOrg() {
+		System.out.println("Delete Organization");
+	}
+}
+
